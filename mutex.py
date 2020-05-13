8c1a36247ace8f65de56ce53820180025f4c8d4a
Robbert van Renesse <rv22@cornell.edu>
2020-05-13 01:27:37 -0400

    Better mutual exclusion testing

--- mutex.py
+++ mutex.py
@@ -3,7 +3,7 @@ import cxl
 
 # check to see if this state is bad
 def mutex(state):
-    cs = cxl.findbreak(state.code, state.labels["cs"])
+    cs = state.labels["cs"]
     cnt = 0
     for ctx in state.ctxbag.keys():
         if ctx.pc == cs:
@@ -12,8 +12,7 @@ def mutex(state):
 
 def main():
     (code, labels) = cxl.compile(sys.stdin, "<stdin>")
-    pc = cxl.findbreak(code, labels["cs"])
-    print("BREAK", pc)
+    pc = labels["cs"]
     cxl.run(code, labels, mutex, [ (("p", 0), pc), (("p", 1), pc) ])
 
 if __name__ == "__main__":
