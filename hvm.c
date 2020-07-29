477c60a52c6cfc54d8dbe7d0a51b29943da647c7
Robbert van Renesse <rv22@cornell.edu>
2020-07-29 16:28:25 -0400

    instructions

--- hvm.c
+++ hvm.c
@@ -2,6 +2,23 @@
 #include <stdlib.h>
 #include <string.h>
 
+/*
+    Step 1: read the entire "program" into an array.
+        At the end, each element in the array will have
+        an "Op" object in it, subclassed into PushOp,
+        StoreOp, ReturnOp, etc.
+
+    Step 2: execute the program, starting at program counter 0
+        You'll need:
+            - a program counter (an integer)
+            - a stack (of Value objects, subclassed into IntValue, etc.)
+            - a stack pointer, initially pointing to the top of the stack
+            - a map of StringValue to Value to hold the variables.
+                For example, this maps "x" to 3, say.
+                Store operations put something in the map.  Load operations
+                take something out of the map.
+*/
+
 int main(){
     int stack[100];
     int *sp = &stack[100];
