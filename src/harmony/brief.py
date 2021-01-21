d4b55d07cf60da7ea6626c1ffcba0981773800c3
Robbert van Renesse <rv22@cornell.edu>
2021-01-21 17:47:32 -0500

    Latest

---
+++ src/harmony/brief.py
@@ -0,0 +1,130 @@
+def brief_kv(js):
+    return (brief_string(js["key"]), brief_string(js["value"]))
+
+def brief_idx(js):
+    if js["type"] == "atom":
+        return brief_string(js)
+    return "[" + brief_string(js) + "]"
+
+def brief_string(js):
+    type = js["type"]
+    v = js["value"]
+    if type in { "bool", "int" }:
+        return v
+    if type == "atom":
+        return "." + v
+    if type == "set":
+        if v == []:
+            return "{}"
+        return "{ " + ", ".join(v) + " }"
+    if type == "dict":
+        if v == []:
+            return "()"
+        lst = [ brief_kv(kv) for kv in v ]
+        keys = [ k for k,v in lst ]
+        if keys == [str(i) for i in range(len(v))]:
+            return "[ " + ", ".join([v for k,v in lst]) + " ]" 
+        else:
+            return "dict{ " + ", ".join([k + ": " + v for k,v in lst]) + " }" 
+    if type == "pc":
+        return "PC(%s)"%v
+    if type == "address":
+        if v == []:
+            return "None"
+        return "?" + v[0]["value"] + "".join([ brief_idx(kv) for kv in v[1:] ])
+    if type == "context":
+        return "CONTEXT(" + brief_string(v["name"]) + ")"
+
+def brief_print_vars(d):
+    print("{", end="")
+    first = True
+    for k, v in d.items():
+        if first:
+            first = False
+        else:
+            print(",", end="")
+        print(" %s: %s"%(k, brief_string(v)), end="")
+    print(" }")
+
+def brief_print_range(start, end):
+    if start == end:
+        return "%d"%(start)
+    if start + 1 == end:
+        return "%d,%d"%(start, end)
+    return "%d-%d"%(start, end)
+
+class Brief:
+    def __init__(self):
+        self.tid = None
+        self.name = None
+        self.start = 0
+        self.steps = ""
+        self.interrupted = False
+        self.lastmis = {}
+        self.shared = {}
+        self.failure = ""
+
+    def flush(self):
+        if self.tid != None:
+            print("T%s: %s ["%(self.tid, self.name), end="")
+            if self.steps != "":
+                self.steps += ","
+            self.steps += brief_print_range(self.start, int(self.lastmis["pc"]))
+            print(self.steps + "] ", end="");
+            brief_print_vars(self.shared);
+
+    def print_macrostep(self, mas):
+        mis = mas["microsteps"]
+        if mas["tid"] != self.tid:
+            self.flush()
+            self.tid = mas["tid"]
+            self.name = mas["name"]
+            self.interrupted = False
+            self.lastmis = mis[0]
+            self.start = int(self.lastmis["pc"])
+            if "shared" in self.lastmis:
+                self.shared = self.lastmis["shared"]
+            lastpc = 0
+            self.steps = ""
+            begin = 1
+        else:
+            begin = 0
+        for i in range(begin, len(mis)):
+            if "shared" in mis[i]:
+                self.shared = mis[i]["shared"]
+            if self.interrupted:
+                if self.steps != "":
+                    self.steps += ","
+                self.steps += brief_print_range(self.start, int(self.lastmis["pc"]))
+                self.start = int(mis[i]["pc"])
+                self.steps += ",interrupt"
+            elif "choose" in mis[i]:
+                if self.steps != "":
+                    self.steps += ","
+                self.steps += brief_print_range(self.start, int(mis[i]["pc"]))
+                self.steps += "(choose %s)"%brief_string(mis[i]["choose"])
+                self.start = int(mis[i]["pc"]) + 1
+            elif int(mis[i]["pc"]) != int(self.lastmis["pc"]) + 1:
+                if self.steps != "":
+                    self.steps += ","
+                self.steps += brief_print_range(self.start, int(self.lastmis["pc"]))
+                self.start = int(mis[i]["pc"])
+            self.lastmis = mis[i]
+            if "failure" in self.lastmis:
+                self.failure = self.lastmis["failure"]
+            self.interrupted = "interrupt" in self.lastmis and self.lastmis["interrupt"] == "True"
+
+    def run(self):
+        with open("charm.json") as f:
+            top = json.load(f)
+            assert isinstance(top, dict)
+            if top["issue"] == "No issues":
+                return True
+
+            # print("Issue:", top["issue"])
+            assert isinstance(top["macrosteps"], list)
+            for mes in top["macrosteps"]:
+                self.print_macrostep(mes)
+            self.flush()
+            print(self.failure)
+            return False;
---
+++ src/harmony/genhtml.py
@@ -0,0 +1,329 @@
+class GenHTML:
+    def __init__(self):
+        self.top = {}
+        self.nmegasteps = 0
+        self.nmicrosteps = 0
+        self.nthreads = 0
+        self.vardir = {}
+
+        self.style = """
+m4_include(charm.css)
+        """
+        self.js = """
+m4_include(charm.js)
+        """
+
+    def json_kv(self, js):
+        return json_string(js["key"]) + ": " + json_string(js["value"])
+
+    def json_idx(self, js):
+        if js["type"] == "atom":
+            return json_string(js)
+        return "[" + self.json_string(js) + "]"
+
+    def json_string(self, js):
+        type = js["type"]
+        v = js["value"]
+        if type in { "bool", "int" }:
+            return v
+        if type == "atom":
+            return "." + v
+        if type == "set":
+            if v == []:
+                return "{}"
+            return "{ " + ", ".join(v) + " }"
+        if type == "dict":
+            if v == []:
+                return "()"
+            return "dict{ " + ", ".join([ self.json_kv(kv) for kv in v ]) + " }" 
+        if type == "pc":
+            return "PC(%s)"%v
+        if type == "address":
+            if v == []:
+                return "None"
+            return "?" + v[0]["value"] + "".join([ self.json_idx(kv) for kv in v[1:] ])
+        if type == "context":
+            return "CONTEXT(" + self.json_string(v["name"]) + ")"
+        assert False
+
+    def file_include(self, name, f):
+        with open(name) as g:
+            print(g.read(), file=f)
+
+    def html_megastep(self, step, tid, name, nmicrosteps, width, f):
+        print("<tr id='mes%d'>"%(step-1), file=f)
+        print("  <td align='right'>", file=f)
+        print("    %d&nbsp;"%step, file=f)
+        print("  </td>", file=f)
+
+        print("  <td>", file=f)
+        print("    T%s: %s"%(tid, name), file=f, end="")
+        print("  </td>", file=f)
+
+        print("  <td>", file=f)
+        time = nmicrosteps
+        nrows = (time + 29) // 30
+        print("    <canvas id='timeline%d' width='300px' height='%dpx'>"%(step-1, 10*nrows), file=f)
+        print("    </canvas>", file=f)
+        print("  </td>", file=f)
+
+        print("  <td align='center'>", file=f);
+        print("  </td>", file=f)
+
+        for i in range(width):
+          print("  <td align='center'>", file=f)
+          print("  </td>", file=f)
+        print("</tr>", file=f)
+
+    def vardim(self, d):
+        totalwidth = 0
+        maxheight = 0
+        if isinstance(d, dict):
+            for k in sorted(d.keys()):
+                (w, h) = self.vardim(d[k])
+                totalwidth += w
+                if h + 1 > maxheight:
+                    maxheight = h + 1
+        else:
+            return (1, 0)
+        return (totalwidth, maxheight)
+
+    def varhdr(self, d, name, nrows, f):
+        q = queue.Queue()
+        level = 0
+        q.put((d, level))
+        while not q.empty():
+            (nd, nl) = q.get()
+            if nl > level:
+                print("</tr><tr>", file=f)
+                level = nl
+            if isinstance(nd, dict):
+                for k in sorted(nd.keys()):
+                    (w,h) = self.vardim(nd[k])
+                    if h == 0:
+                        print("<td align='center' style='font-style: italic' colspan='%d' rowspan='%d'>%s</td>"%(w,nrows-nl,k), file=f)
+                    else:
+                        print("<td align='center' style='font-style: italic' colspan='%d'>%s</td>"%(w,k), file=f)
+                    q.put((nd[k], nl+1))
+
+    def html_top(self, f):
+        (width, height) = self.vardim(self.vardir)
+        print("<table border='1'>", file=f)
+        print("  <thead>", file=f)
+        print("    <tr>", file=f)
+        print("      <th colspan='4' style='color:red;'>", file=f)
+        print("        Issue:", self.top["issue"], file=f)
+        print("      </th>", file=f)
+        print("      <th align='center' colspan='%d'>"%width, file=f)
+        print("        Shared Variables", file=f)
+        print("      </th>", file=f)
+        print("    </tr>", file=f)
+
+        print("    <tr>", file=f)
+        print("      <th align='center' rowspan='%d'>"%height, file=f)
+        print("        Step", file=f)
+        print("      </th>", file=f)
+        print("      <th align='center' rowspan='%d'>"%height, file=f)
+        print("        Thread", file=f)
+        print("      </th>", file=f)
+        print("      <th align='center' rowspan='%d'>"%height, file=f)
+        print("        Instructions Executed", file=f)
+        print("      </th>", file=f)
+        print("      <th align='center' rowspan='%d'>"%height, file=f)
+        print("        &nbsp;PC&nbsp;", file=f)
+        print("      </th>", file=f)
+        self.varhdr(self.vardir, "", height, f)
+        print("    </tr>", file=f)
+        print("  </thead>", file=f)
+
+        print("  <tbody id='mestable'>", file=f)
+        assert isinstance(self.top["macrosteps"], list)
+        nsteps = 0
+        tid = None
+        name = None
+        nmicrosteps = 0
+        for mas in self.top["macrosteps"]:
+            if tid == mas["tid"]:
+                nmicrosteps += len(mas["microsteps"])
+            else:
+                if tid != None:
+                    self.html_megastep(nsteps, tid, name, nmicrosteps, width, f)
+                nsteps += 1
+                tid = mas["tid"]
+                name = mas["name"]
+                nmicrosteps = len(mas["microsteps"])
+        self.html_megastep(nsteps, tid, name, nmicrosteps, width, f)
+        print("  </tbody>", file=f)
+        print("</table>", file=f)
+
+    def html_botleft(self, f):
+        print("<div id='table-wrapper'>", file=f)
+        print("  <div id='table-scroll'>", file=f)
+        print("    <table border='1'>", file=f)
+        print("      <tbody>", file=f)
+        alter = False;
+        for pc, instr in enumerate(self.top["code"]):
+            if str(pc) in self.top["locations"]:
+                alter = not alter;
+            print("        <tr id='P%d'>"%pc, file=f)
+            print("          <td align='right'>", file=f)
+            print("            <a name='P%d'>%d</a>&nbsp;"%(pc, pc), file=f)
+            print("          </td>", file=f)
+            print("          <td style='background-color: %s;'>"%("#E6E6E6" if alter else "white"), file=f)
+            print("            <span title='%s' id='C%d'>"%(self.top["explain"][pc], pc), file=f)
+            print("              %s"%instr, file=f);
+            print("            </span>", file=f)
+            print("          </td>", file=f)
+            print("        </tr>", file=f)
+        print("      </body>", file=f)
+        print("    </table>", file=f)
+        print("  </div>", file=f)
+        print("</div>", file=f)
+
+    def html_botright(self, f):
+        print("<table border='1' id='threadtable'>", file=f)
+        print("  <thead>", file=f)
+        print("    <tr>", file=f)
+        print("      <th>", file=f)
+        print("        Thread", file=f)
+        print("      </th>", file=f)
+        print("      <th>", file=f)
+        print("        Status", file=f)
+        print("      </th>", file=f)
+        print("      <th>", file=f)
+        print("        Stack Trace", file=f)
+        print("      </th>", file=f)
+        print("    </tr>", file=f)
+        print("  </thead>", file=f)
+        print("  <tbody>", file=f)
+        maxtid = 0
+        for i in range(self.nthreads):
+            print("    <tr id='thread%d'>"%i, file=f)
+            print("      <td align='center'>", file=f)
+            print("        T%d"%i, file=f)
+            print("      </td>", file=f)
+            print("      <td align='center'>", file=f)
+            print("        init", file=f)
+            print("      </td>", file=f)
+            print("      <td>", file=f)
+            print("        <table id='threadinfo%d' border='1'>"%i, file=f)
+            print("        </table>", file=f)
+            print("      </td>", file=f)
+            print("    </tr>", file=f)
+        print("  </tbody>", file=f)
+        print("</table>", file=f)
+
+    def html_outer(self, f):
+        print("<table>", file=f)
+        print("  <tr>", file=f)
+        print("    <td colspan='2'>", file=f)
+        self.html_top(f)
+        print("    </td>", file=f)
+        print("  </tr>", file=f)
+        print("  <tr>", file=f)
+        print("    <td colspan='2'>", file=f)
+        print("      <h3 style='color:blue;' id='coderow'>CODE GOES HERE</h3>", file=f)
+        print("    </td>", file=f)
+        print("  </tr>", file=f)
+        print("  <tr>", file=f)
+        print("    <td valign='top'>", file=f)
+        self.html_botleft(f)
+        print("    </td>", file=f)
+        print("    <td valign='top'>", file=f)
+        self.html_botright(f)
+        print("    </td>", file=f)
+        print("  </tr>", file=f)
+        print("</table>", file=f)
+
+    def vardir_dump(self, d, path, index, f):
+        if isinstance(d, dict):
+            for k in sorted(d.keys()):
+                index = self.vardir_dump(d[k], path + [k], index, f)
+            return index
+        if index > 0:
+            print(",", file=f)
+        print("  " + str(path), end="", file=f)
+        return index + 1
+
+    def html_script(self, f):
+        print("<script>", file=f)
+        print("var nthreads = %d;"%self.nthreads, file=f)
+        print("var nmegasteps = %d;"%self.nmegasteps, file=f)
+        print("var vardir = [", file=f)
+        self.vardir_dump(self.vardir, [], 0, f)
+        print(file=f)
+        print("];", file=f)
+        print("var state =", file=f)
+        self.file_include("charm.json", f)
+        print(";", file=f)
+        print(self.js, file=f)
+        # file_include("charm.js", f)
+        print("</script>", file=f)
+
+    def html_body(self, f):
+        print("<body>", file=f)
+        self.html_outer(f)
+        self.html_script(f)
+        print("</body>", file=f)
+
+    def html_head(self, f):
+        print("<head>", file=f)
+        print("  <style>", file=f)
+        print(self.style, file=f)
+        print("  </style>", file=f)
+        print("</head>", file=f)
+
+    def html(self, f):
+        print("<html>", file=f)
+        self.html_head(f)
+        self.html_body(f)
+        print("</html>", file=f)
+
+    def var_convert(self, v):
+        if v["type"] != "dict":
+            return self.json_string(v)
+        d = {}
+        for kv in v["value"]:
+            k = self.json_string(kv["key"])
+            d[k] = self.var_convert(kv["value"])
+        return d
+
+    def dict_merge(self, vardir, d):
+        for (k, v) in d.items():
+            if not isinstance(v, dict):
+                vardir[k] = v
+            elif v != {}:
+                if k not in vardir:
+                    vardir[k] = {}
+                elif not isinstance(vardir[k], dict):
+                    continue
+                self.dict_merge(vardir[k], v)
+
+    def vars_add(self, vardir, shared):
+        d = {}
+        for (k, v) in shared.items():
+            d[k] = self.var_convert(v)
+        self.dict_merge(vardir, d)
+
+    def run(self):
+        # First figure out how many megasteps there are and how many threads
+        lasttid = -1
+        with open("charm.json") as f:
+            glob = GenHTML()
+            self.top = json.load(f)
+            assert isinstance(self.top, dict)
+            macrosteps = self.top["macrosteps"]
+            for mas in macrosteps:
+                tid = int(mas["tid"])
+                if tid >= self.nthreads:
+                    self.nthreads = tid + 1
+                if tid != lasttid:
+                    self.nmegasteps += 1
+                    lasttid = tid
+                self.nmicrosteps += len(mas["microsteps"])
+                for mis in mas["microsteps"]:
+                    if "shared" in mis:
+                        self.vars_add(self.vardir, mis["shared"])
+
+        with open("harmony.html", "w") as out:
+            self.html(out)
