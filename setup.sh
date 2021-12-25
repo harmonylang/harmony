314aea744f10a744b8e308445d05c29f62e99529
Anthony Yang <contact@anthonyyang.dev>
2021-12-25 01:53:52 -0500

    update setup

--- setup.sh
+++ setup.sh
@@ -8,13 +8,8 @@ turn_into_package() {
 
 rm -rf "$PACKAGE_DIR.egg-info"
 
-cp -r code/ "$PACKAGE_DIR"
-
 turn_into_package "$PACKAGE_DIR/code"
 turn_into_package "$PACKAGE_DIR/modules"
-
-# Copy relevant files into published package directory
-cp iface.py dfacmp.py charm.Windows.exe "$PACKAGE_DIR"
 
 # harmony and harmony.bat do not need to be copied
 # as pip creates a console script when the package is installed.
