5e1d9f57b6cd3f41351d25f83ab9438334a11c1a
Anthony Yang <contact@anthonyyang.dev>
2022-01-05 23:02:23 -0500

    update compiler integration test

--- compiler_integration_test.py
+++ compiler_integration_test.py
@@ -1,288 +1,148 @@
 import subprocess
 import sys
 import time
-from typing import List, TextIO, Optional, Union
+import pathlib
+import dataclasses
+from typing import List, Set, TextIO, Optional, Union
 
 
+@dataclasses.dataclass
 class TestCase:
+    filename: str
+    harmony_args: List[str] = None
+    lines_to_check: Union[List[int], str] = 'all'
+    is_ephemeral: bool = False
 
-    def __init__(
-        self,
-        harmony_args: str,
-        lines_to_check: Union[List[int], str] = 'all',
-        is_ephemeral: bool = False
-    ):
-        self.harmony_args = harmony_args
-        self.lines_to_check = lines_to_check
-        self.is_ephemeral = is_ephemeral
-
-
+@dataclasses.dataclass
 class ExecutionResult:
+    output: str
+    average_duration: float
 
-    def __init__(
-        self,
-        test_case: TestCase,
-        expected_output: str,
-        baseline_output: str,
-        average_duration: Optional[float] = None
-    ):
-        self.test_case = test_case
-        self.expected_output = expected_output
-        self.baseline_output = baseline_output
-        self.average_duration = average_duration
+@dataclasses.dataclass
+class TestResult:
+    test_case: TestCase
+    current: ExecutionResult
+    baseline: ExecutionResult
 
+@dataclasses.dataclass
+class ComparisonResult:
+    test_result: TestResult
+    output_expected: bool
+    duration_expected: bool
 
-# negative values in lines_to_check index lines from the end of the expected
-# output
-TEST_CASES: List[TestCase] = [
-    TestCase('code/prog1.hny'),
-    TestCase('code/prog2.hny', lines_to_check=[0, 1]),
-    TestCase('code/Up.hny'),
-    TestCase('code/UpEnter.hny'),
-    TestCase('code/csbarebones.hny'),
-    TestCase('code/cs.hny'),
-    TestCase('code/naiveLock.hny'),
-    TestCase('code/naiveFlags.hny'),
-    TestCase('code/naiveTurn.hny'),
-    TestCase('code/Peterson.hny'),
-    TestCase('code/PetersonInductive.hny'),
-    TestCase('code/csonebit.hny'),
-    TestCase('code/PetersonMethod.hny'),
-    TestCase('code/spinlock.hny'),
-    TestCase('code/csTAS.hny'),
-    TestCase('code/UpLock.hny'),
-    TestCase('-msynch=synchS code/UpLock.hny'),
-    TestCase('code/xy.hny', lines_to_check=[0, 1, -1]),
-    TestCase('code/atm.hny', lines_to_check=[0, 1]),
-    TestCase('code/queuedemo.hny'),
-    TestCase('-msynch=synchS code/queuedemo.hny'),
-    TestCase('code/qtestseq.hny'),
-    TestCase('code/qtest1.hny'),
-    TestCase('-msynch=synchS code/qtest1.hny'),
-    TestCase('code/qtest2.hny'),
-    TestCase('-msynch=synchS code/qtest2.hny'),
-    TestCase('code/qtest4.hny'),
-    TestCase('-mqueue=queueMS code/queuedemo.hny', lines_to_check=[0, 1, 2]),
-    TestCase('-mqueue=queueMS -msynch=synchS code/queuedemo.hny', lines_to_check=[0, 1, 2]),
-    TestCase('code/lltest.hny'),
-    TestCase('-msynch=synchS code/lltest.hny'),
-    TestCase('code/RWtest.hny'),
-    TestCase('-msynch=synchS code/RWtest.hny'),
-    TestCase('-mRW=RWsbs code/RWtest.hny'),
-    TestCase('-mRW=RWsbs -msynch=synchS code/RWtest.hny'),
-    TestCase('-mRW=RWfair code/RWtest.hny'),
-    TestCase('-mRW=RWfair -msynch=synchS code/RWtest.hny'),
-    TestCase('code/BBhoaretest.hny'),
-    TestCase('-msynch=synchS code/BBhoaretest.hny'),
-    TestCase('-mRW=RWcv code/RWtest.hny'),
-    TestCase('-mRW=RWcv -msynch=synchS code/RWtest.hny'),
-    TestCase('code/qsorttest.hny'),
-    TestCase('code/Diners.hny'),
-    TestCase('-msynch=synchS code/Diners.hny'),
-    TestCase('code/DinersAvoid.hny'),
-    TestCase('code/bank.hny'),
-    TestCase('code/counter.hny'),
-    TestCase('code/qbarrier.hny'),
-    TestCase('-msynch=synchS code/qbarrier.hny'),
-    TestCase('code/barriertest.hny'),
-    TestCase('-msynch=synchS code/barriertest.hny'),
-    TestCase('code/trap.hny'),
-    TestCase('code/trap2.hny'),
-    TestCase('code/trap3.hny'),
-    TestCase('code/trap4.hny'),
-    TestCase('code/trap5.hny'),
-    TestCase('code/trap6.hny'),
-    TestCase('-msynch=synchS code/trap6.hny'),
-    TestCase('code/hw.hny'),
-    TestCase('code/abptest.hny'),
-    TestCase('test/DinersVar.hny'),
-]
+def load_test_cases() -> List[TestCase]:
+    modules_dir = pathlib.Path("harmony_model_checker") / "modules"
+    runall_script = pathlib.Path("runall.scr")
+    code_dir = pathlib.Path("code")
+    test_dir = pathlib.Path("test")
 
+    test_cases: List[TestCase] = []
 
-def find_test_case(harmony_args: str) -> Optional[TestCase]:
-    for test_case in TEST_CASES:
-        if test_case.harmony_args == harmony_args:
-            return test_case
-    return None
+    # Ensure our modules compile correctly
+    tested_files: Set[str] = set()
+    for f in modules_dir.glob("*.hny"):
+        tested_files.add(str(f.resolve()))
+        test_cases.append(TestCase(filename=str(f)))
 
+    with runall_script.open("r") as f:
+        # Exclude the first arg, i.e. the harmony script filename
+        all_tc = [l.strip().split() for l in f.readlines() if l.startswith("./harmony")]
+        for tc in all_tc:
+            filename = pathlib.Path(tc[-1])
+            if filename.exists():
+                tested_files.add(str(filename.resolve()))
+                test_cases.append(TestCase(
+                    filename=str(filename),
+                    harmony_args=tc[:-1]
+                ))
 
-def find_execution_result(
-    execution_results: List[ExecutionResult],
-    harmony_args: str
-) -> Optional[ExecutionResult]:
+    for f in code_dir.glob("*.hny"):
+        if str(f.resolve()) not in tested_files and f.exists():
+            tested_files.add(str(f.resolve()))
+            test_cases.append(TestCase(filename=str(f)))
+    
+    for f in test_dir.glob("*.hny"):
+        if str(f.resolve()) not in tested_files and f.exists():
+            tested_files.add(str(f.resolve()))
+            test_cases.append(TestCase(filename=str(f)))
 
-    for execution_result in execution_results:
-        if execution_result.test_case.harmony_args == harmony_args:
-            return execution_result
-    return None
+    return test_cases
 
+TEST_CASES = load_test_cases()
+print(f"Number of tests: {len(TEST_CASES)}")
 
-# def load_results(f: TextIO) -> List[ExecutionResult]:
-#     state = 'header'
-#     results: List[ExecutionResult] = []
+def dump_result(result: ComparisonResult, f: TextIO):
+    test_case = result.test_result.test_case
+    baseline = result.test_result.baseline
+    current = result.test_result.current
 
-#     average_duration: Optional[float] = None
-#     expected_output: str = ''
-#     harmony_args: str = ''
-
-#     def init_execution_result():
-#         nonlocal average_duration, expected_output, harmony_args
-
-#         average_duration = None
-#         expected_output = ''
-#         harmony_args = ''
-
-#     init_execution_result()
-
-#     for line in f:
-#         invalid_line = False
-
-#         if state == 'header':
-#             line = line.strip()
-#             if not line:
-#                 continue
-
-#             if line == '# Integration Test Results':
-#                 state = 'top level'
-#             else:
-#                 invalid_line = True
-
-#         elif state == 'top level':
-#             line = line.strip()
-#             if not line:
-#                 continue
-
-#             if line == '---':
-#                 if harmony_args and expected_output:
-#                     test_case = find_test_case(harmony_args) \
-#                         or TestCase(harmony_args=harmony_args, is_ephemeral=True)
-
-#                     if test_case.is_ephemeral:
-#                         print(f'Found ephemeral test case while parsing: {harmony_args}')
-
-#                     results.append(ExecutionResult(
-#                         test_case,
-#                         expected_output,
-#                         average_duration
-#                     ))
-
-#                 init_execution_result()
-
-#             elif line.startswith('###'):
-#                 harmony_args = line[3:].strip()
-
-#             elif line.startswith('Average duration:'):
-#                 try:
-#                     average_duration = float(line[len('Average duration:'):].strip())
-#                 except ValueError:
-#                     invalid_line = True
-
-#             elif line == 'Expected output:':
-#                 state = 'expected output quote start'
-
-#             else:
-#                 invalid_line = True
-
-#         elif state == 'expected output quote start':
-#             line = line.strip()
-#             if not line:
-#                 continue
-
-#             if line == '```':
-#                 expected_output = ''
-#                 state = 'expected output body'
-#             else:
-#                 invalid_line = True
-
-#         elif state == 'expected output body':
-#             if line == '```\n':
-#                 state = 'top level'
-#             else:
-#                 expected_output += line
-
-#         else:
-#             raise ValueError(f'Invalid parser state: {state}')
-
-#         if invalid_line:
-#             raise ValueError(f'Unexpected line: "{line}". Parser state: {state}')
-
-#     return results
-
-
-def dump_result(result: ExecutionResult, f: TextIO):
     f.write('---')
     f.write('\n')
-    f.write(f'### {result.test_case.harmony_args}\n')
+    f.write(f'### {test_case.harmony_args or ""} {test_case.filename}\n')
     f.write('\n')
-    f.write(f'Average duration: {result.average_duration}\n')
+    f.write("#### Summary\n\n")
+    f.write(f"Duration is good: {'✅' if result.duration_expected else '❌'}\n\n")
+    f.write(f"Output is good: {'✅' if result.output_expected else '❌'}\n\n")
+
+    f.write("#### Baseline Output\n\n")
+    f.write(f"```{baseline.output}```\n\nDuration: {baseline.average_duration}\n\n")
+
+    f.write("#### Current Output\n\n")
+    f.write(f"```{current.output}```\n\nDuration: {current.average_duration}\n\n")
+
+def dump_results(results: List[ComparisonResult], f: TextIO):
+    f.write('# Compiler Integration Test Results\n')
     f.write('\n')
-    f.write('Expected output:\n')
-    f.write('```\n')
-    for line in result.expected_output.splitlines():
-        line = line.strip()
-        f.write(line + '\n')
-    f.write('```\n')
-    f.write('\n')
 
-
-def dump_results(results: List[ExecutionResult], f: TextIO):
-    f.write('# Integration Test Results\n')
-    f.write('\n')
-
-    process_after = []
-
-    for test_case in TEST_CASES:
-        execution_result = find_execution_result(results, test_case.harmony_args)
-        if execution_result:
-            dump_result(execution_result, f)
-
-        else:
-            process_after.append(execution_result)
-
-    process_after.sort(key=lambda r: r.test_case.harmony_args)
-
-    for execution_result in process_after:
-        dump_result(execution_result, f)
+    for r in results:
+        dump_result(r, f)
 
     f.write('---')
 
 
-def evaluate_test_case(test_case: TestCase, n: int) -> ExecutionResult:
-    print(f'Evaluating `harmony {test_case.harmony_args}`')
+def evaluate_test_case(test_case: TestCase, n: int) -> TestResult:
+    print(f'Evaluating `harmony {test_case.harmony_args} {test_case.filename}`')
 
     durations = []
+    baseline_durations = []
     for _ in range(n):
-        start_time = time.time()
+        start_time = time.process_time()
         result = subprocess.run(
-            f'harmony {test_case.harmony_args}'.split(),
+            f'harmony {test_case.harmony_args or ""} {test_case.filename}'.split(),
             capture_output=True,
             encoding='utf8'
         )
+        durations.append(time.process_time() - start_time)
+        
+        start_time = time.process_time()
         baseline_result = subprocess.run(
-            f'python harmony.py {test_case.harmony_args}'.split(),
+            f'python harmony.py {test_case.harmony_args or ""} {test_case.filename}'.split(),
             capture_output=True,
             encoding='utf8'
         )
-        durations.append(time.time() - start_time)
+        baseline_durations.append(time.process_time() - start_time)
 
-    assert type(result.stdout) == str
+    assert isinstance(result.stdout, str) and isinstance(baseline_result.stdout, str)
 
     average_duration = sum(durations) / len(durations)
+    average_baseline_duration = sum(baseline_durations) / len(baseline_durations)
 
-    return ExecutionResult(
-        test_case,
-        expected_output=result.stdout,
-        baseline_output=baseline_result.stdout,
-        average_duration=average_duration
+    current_execution = ExecutionResult(result.stdout, average_duration)
+    baseline_execution = ExecutionResult(baseline_result.stdout, average_baseline_duration)
+
+    return TestResult(
+        test_case=test_case,
+        current=current_execution,
+        baseline=baseline_execution
     )
 
 
-def evaluate_test_cases(test_cases: List[TestCase], n: int) -> List[ExecutionResult]:
+def evaluate_and_compare_test_cases(test_cases: List[TestCase], n: int) -> List[ComparisonResult]:
     curr_results = []
 
     for test_case in test_cases:
-        curr_results.append(evaluate_test_case(test_case, n))
-
+        tr = evaluate_test_case(test_case, n)
+        curr_results.append(compare_result(tr))
     return curr_results
 
 
@@ -324,17 +184,19 @@ def expected_outputs_match(
 
 
 def merge_expected_outputs(
-    test_case: TestCase,
-    prev: str,
-    curr: str
-) -> str:
+    result: TestResult
+) -> bool:
+
+    test_case = result.test_case
+    prev = result.baseline.output
+    curr = result.current.output
 
     if expected_outputs_match(
         test_case,
         prev,
         curr
     ):
-        return prev
+        return True
 
     print(f'{test_case.harmony_args}: difference on expected_output')
     print('Previous results expected output:')
@@ -346,92 +208,61 @@ def merge_expected_outputs(
     print(curr)
     print('```')
 
-    if input('Take current expected output? [y/N]: ') == 'y':
-        print('Taking current expected output')
+    if input('Are the outputs satisfactory? [y/N]: ') == 'y':
+        print('Accepting the current output')
         print()
-        return curr
+        return True
 
     else:
-        print('Leaving previous expected output')
+        print('Output is not satisfactory')
         print()
-        return prev
+        return False
 
 
 def merge_average_durations(
-    test_case: TestCase,
-    prev: Optional[float],
-    curr: Optional[float]
-) -> Optional[float]:
+    result: TestResult,
+) -> bool:
+
+    test_case = result.test_case
+    prev = result.baseline.average_duration
+    curr = result.current.average_duration
 
     if prev == curr:
-        return prev
+        return True
 
-    if prev and curr:
-        percent_difference = 2 * (abs(prev - curr) / (prev + curr))
+    percent_difference = 2 * (abs(prev - curr) / (prev + curr))
 
-        percent_difference = round(100 * percent_difference, 3)
+    percent_difference = round(100 * percent_difference, 3)
 
-        if percent_difference <= 100:
-            return prev
+    if percent_difference <= 100:
+        return True
 
-        print(f'{test_case.harmony_args}: difference on average duration by {percent_difference}% > 100%')
-        print(f'Previous duration: {prev}')
-        print(f'Current duration: {curr}')
+    print(f'{test_case.harmony_args}: difference on average duration by {percent_difference}% > 100%')
+    print(f'Previous duration: {prev}')
+    print(f'Current duration: {curr}')
+    print()
+    if input('Take current average duration? [y/N]: ') == 'y':
+        print('Taking current average duration')
         print()
-        if input('Take current average duration? [y/N]: ') == 'y':
-            print('Taking current average duration')
-            print()
-            return curr
-
-        else:
-            print('Leaving previous average duration')
-            print()
-            return prev
-
-    elif prev and not curr:
-        return prev
+        return True
 
     else:
-        return None
+        print('Leaving previous average duration')
+        print()
+        return False
 
 
-def compare_results(
-    curr_results: List[ExecutionResult]
-) -> List[ExecutionResult]:
+def compare_result(
+    result: TestResult
+) -> ComparisonResult:
+    output_is_expected = merge_expected_outputs(result)
+    duration_is_expected = merge_average_durations(result)
 
-    # The merge process biases towards keeping the final results equal to the
-    # prev results, except where noted.
-    #
-    # The merge process should not depend on the list of all test cases, since
-    # it may be useful to merge two arbitrary results lists in the future.
-
-    final_results: List[ExecutionResult] = []
-
-    for curr_result in curr_results:
-        # harmony_args = curr_result.test_case.harmony_args
-
-        test_case = curr_result.test_case
-
-        final_expected_outputs = merge_expected_outputs(
-            test_case,
-            curr_result.baseline_output,
-            curr_result.expected_output
-        )
-
-        final_average_duration = merge_average_durations(
-            test_case,
-            curr_result.average_duration,
-            curr_result.average_duration
-        )
-
-        final_results.append(ExecutionResult(
-            test_case,
-            final_expected_outputs,
-            final_expected_outputs,
-            final_average_duration
-        ))
-
-    return final_results
+    return ComparisonResult(
+        test_result=result,
+        output_expected=output_is_expected,
+        duration_expected=duration_is_expected
+    )
 
 
 def main():
@@ -439,22 +270,12 @@ def main():
     print()
 
     if '--full' in sys.argv:
-        curr_results = evaluate_test_cases(TEST_CASES, n=10)
+        final_results = evaluate_and_compare_test_cases(TEST_CASES, n=10)
     else:
-        curr_results = evaluate_test_cases(TEST_CASES, n=1)
+        final_results = evaluate_and_compare_test_cases(TEST_CASES, n=1)
     print()
 
-    # try:
-    #     with open('hintegration_results.md') as f:
-    #         prev_results = load_results(f)
-    # except FileNotFoundError:
-    #     with open('hintegration_results.md', 'w') as f:
-    #         dump_results(curr_results, f)
-    #     return
-
-    final_results = compare_results(curr_results)
-
-    with open('hintegration_results.md', 'w') as f:
+    with open('compiler_integration_results.md', 'w') as f:
         dump_results(final_results, f)
 
 
