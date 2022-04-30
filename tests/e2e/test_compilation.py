6aaa1a4498e36686b851dd3a3563a196ec0b96b3
Anthony Yang <ay339@cornell.edu>
2022-04-30 15:51:46 -0400

    Write tests with pytest

--- tests/e2e/test_compilation.py
+++ tests/e2e/test_compilation.py
@@ -1,39 +1,41 @@
 
 from datetime import timedelta
 import pathlib
+import time
 from typing import List, NamedTuple
-import unittest
+import pytest
 
 import harmony_model_checker.harmony.harmony as legacy_harmony
 from harmony_model_checker.compile import do_compile
 
-class Test(NamedTuple):
+class Params(NamedTuple):
     filename: pathlib.Path
     max_time: timedelta
 
-def load_harmony_files() -> List[Test]:
+def load_harmony_files() -> List[Params]:
     code_dir = pathlib.Path(__file__).parent.parent.parent / "code"
     return [
-        Test(
+        Params(
             filename=f,
             max_time=timedelta(milliseconds=500)
         ) for f in code_dir.glob("*.hny")
     ]
 
-class TestCompilation(unittest.TestCase):
-    def test_compilation(self):
-        for h in load_harmony_files():
-            with self.subTest(msg=f"Compiling {str(h.filename)}"):
-                do_compile(str(h.filename), consts=[], mods=[], interface=None)
-                legacy_harmony.files.clear()           # files that have been read already
-                legacy_harmony.modules.clear()         # modules modified with -m
-                legacy_harmony.used_modules.clear()    # modules modified and used
-                legacy_harmony.namestack.clear()       # stack of module names being compiled
+@pytest.fixture(autouse=True)
+def run_around_tests():
+    legacy_harmony.files.clear()           # files that have been read already
+    legacy_harmony.modules.clear()         # modules modified with -m
+    legacy_harmony.used_modules.clear()    # modules modified and used
+    legacy_harmony.namestack.clear()       # stack of module names being compiled
 
-                legacy_harmony.imported.clear()
-                legacy_harmony.constants.clear()
-                legacy_harmony.used_constants.clear()
+    legacy_harmony.imported.clear()
+    legacy_harmony.constants.clear()
+    legacy_harmony.used_constants.clear()
+    yield
 
-
-if __name__ == "__main__":
-    unittest.main()
+@pytest.mark.parametrize("param", load_harmony_files())
+def test_compilation(param):
+    start_time = time.perf_counter_ns()
+    do_compile(str(param.filename), consts=[], mods=[], interface=None)
+    end_time = time.perf_counter_ns()
+    assert end_time - start_time <= param.max_time.microseconds * 1000
