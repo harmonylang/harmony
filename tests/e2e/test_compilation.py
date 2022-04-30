4162a55aaaef959e231fadbf38946bd980d1a7c9
Anthony Yang <ay339@cornell.edu>
2022-04-30 15:18:44 -0400

    Clear global values after each compilation

--- tests/e2e/test_compilation.py
+++ tests/e2e/test_compilation.py
@@ -4,6 +4,7 @@ import pathlib
 from typing import List, NamedTuple
 import unittest
 
+import harmony_model_checker.harmony.harmony as legacy_harmony
 from harmony_model_checker.compile import do_compile
 
 class Test(NamedTuple):
@@ -15,15 +16,24 @@ def load_harmony_files() -> List[Test]:
     return [
         Test(
             filename=f,
-            max_time=timedelta(seconds=1)
+            max_time=timedelta(milliseconds=500)
         ) for f in code_dir.glob("*.hny")
     ]
 
 class TestCompilation(unittest.TestCase):
     def test_compilation(self):
         for h in load_harmony_files():
-            with self.subTest(msg=f"Checking {str(h.filename)}"):
+            with self.subTest(msg=f"Compiling {str(h.filename)}"):
                 do_compile(str(h.filename), consts=[], mods=[], interface=None)
+                legacy_harmony.files.clear()           # files that have been read already
+                legacy_harmony.modules.clear()         # modules modified with -m
+                legacy_harmony.used_modules.clear()    # modules modified and used
+                legacy_harmony.namestack.clear()       # stack of module names being compiled
+
+                legacy_harmony.imported.clear()
+                legacy_harmony.constants.clear()
+                legacy_harmony.used_constants.clear()
+
 
 if __name__ == "__main__":
     unittest.main()
