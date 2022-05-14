650db932257161cda45d3fb053b14ff41a061887
Anthony Yang <ay339@cornell.edu>
2022-05-14 09:57:47 -0400

    Add test for common pipeline

--- tests/e2e/test_compilation.py
+++ tests/e2e/test_compilation.py
@@ -1,58 +1,11 @@
+from load_test_files import *
 
-from datetime import timedelta
-import pathlib
 import time
-from typing import List, NamedTuple
 import pytest
 from harmony_model_checker.exception import HarmonyCompilerError, HarmonyCompilerErrorCollection
 
 import harmony_model_checker.harmony.harmony as legacy_harmony
 from harmony_model_checker.compile import do_compile
-
-class Params(NamedTuple):
-    filename: pathlib.Path
-    max_time: timedelta
-    modules: List[str]
-    constants: List[str]
-
-def load_dir(dir: pathlib.Path, modules=None, constants=None):
-    return [
-        Params(
-            filename=f,
-            max_time=timedelta(seconds=1),
-            modules=modules or [],
-            constants=constants or [],
-        ) for f in dir.glob("*.hny")
-    ]
-
-_DIR = pathlib.Path(__file__).parent
-
-def load_public_harmony_files():
-    code_dir = _DIR.parent.parent / "code"
-    return load_dir(code_dir)
-
-def load_failing_harmony_files():
-    code_dir = _DIR / "errors"
-    return load_dir(code_dir)
-
-def load_constant_defined_harmony_files():
-    code_dir = _DIR / "constants"
-    return load_dir(code_dir, constants=['C=42'])
-
-def load_failing_constant_defined_harmony_files():
-    code_dir = _DIR / "constants"
-    return load_dir(code_dir, constants=['C=']) \
-         + load_dir(code_dir, constants=['C=42', 'A=12'])   # unused constants
-
-def load_module_defined_harmony_files():
-    code_dir = _DIR / "modules"
-    return load_dir(code_dir, modules=['math=resources/math'])
-
-def load_failing_module_defined_harmony_files():
-    code_dir = _DIR / "modules"
-    return load_dir(code_dir, modules=['math=resources/matt']) \
-         + load_dir(code_dir, modules=['math=resources/math',
-                                       'numpy=resources/numpy']) # unused modules
 
 @pytest.fixture(autouse=True)
 def run_around_tests():
