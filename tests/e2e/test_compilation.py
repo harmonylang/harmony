dcbfaa5ca819c5ba40329025636ae092eaec85ea
Anthony Yang <ay339@cornell.edu>
2022-05-14 09:31:04 -0400

    Add module/constants-parameterized and failing compilations

--- tests/e2e/test_compilation.py
+++ tests/e2e/test_compilation.py
@@ -4,6 +4,7 @@ import pathlib
 import time
 from typing import List, NamedTuple
 import pytest
+from harmony_model_checker.exception import HarmonyCompilerError, HarmonyCompilerErrorCollection
 
 import harmony_model_checker.harmony.harmony as legacy_harmony
 from harmony_model_checker.compile import do_compile
@@ -11,15 +12,47 @@ from harmony_model_checker.compile import do_compile
 class Params(NamedTuple):
     filename: pathlib.Path
     max_time: timedelta
+    modules: List[str]
+    constants: List[str]
 
-def load_harmony_files() -> List[Params]:
-    code_dir = pathlib.Path(__file__).parent.parent.parent / "code"
+def load_dir(dir: pathlib.Path, modules=None, constants=None):
     return [
         Params(
             filename=f,
-            max_time=timedelta(seconds=1)
-        ) for f in code_dir.glob("*.hny")
+            max_time=timedelta(seconds=1),
+            modules=modules or [],
+            constants=constants or [],
+        ) for f in dir.glob("*.hny")
     ]
+
+_DIR = pathlib.Path(__file__).parent
+
+def load_public_harmony_files():
+    code_dir = _DIR.parent.parent / "code"
+    return load_dir(code_dir)
+
+def load_failing_harmony_files():
+    code_dir = _DIR / "errors"
+    return load_dir(code_dir)
+
+def load_constant_defined_harmony_files():
+    code_dir = _DIR / "constants"
+    return load_dir(code_dir, constants=['C=42'])
+
+def load_failing_constant_defined_harmony_files():
+    code_dir = _DIR / "constants"
+    return load_dir(code_dir, constants=['C=']) \
+         + load_dir(code_dir, constants=['C=42', 'A=12'])   # unused constants
+
+def load_module_defined_harmony_files():
+    code_dir = _DIR / "modules"
+    return load_dir(code_dir, modules=['math=resources/math'])
+
+def load_failing_module_defined_harmony_files():
+    code_dir = _DIR / "modules"
+    return load_dir(code_dir, modules=['math=resources/matt']) \
+         + load_dir(code_dir, modules=['math=resources/math',
+                                       'numpy=resources/numpy']) # unused modules
 
 @pytest.fixture(autouse=True)
 def run_around_tests():
@@ -33,10 +66,23 @@ def run_around_tests():
     legacy_harmony.used_constants.clear()
     yield
 
-@pytest.mark.parametrize("param", load_harmony_files())
-def test_compilation(param):
+@pytest.mark.parametrize("param", load_public_harmony_files()
+                                + load_constant_defined_harmony_files()
+                                + load_module_defined_harmony_files())
+def test_compilation_success(param):
     start_time = time.perf_counter_ns()
-    do_compile(str(param.filename), consts=[], mods=[], interface=None)
+    do_compile(str(param.filename), consts=param.constants, mods=param.modules, interface=None)
+    end_time = time.perf_counter_ns()
+    duration = end_time - start_time
+    assert duration <= param.max_time.total_seconds() * 1e9
+
+@pytest.mark.parametrize("param", load_failing_harmony_files()
+                                + load_failing_constant_defined_harmony_files()
+                                + load_failing_module_defined_harmony_files())
+def test_compilation_error(param):
+    start_time = time.perf_counter_ns()
+    with pytest.raises((HarmonyCompilerErrorCollection, HarmonyCompilerError)):
+        do_compile(str(param.filename), consts=param.constants, mods=param.modules, interface=None)
     end_time = time.perf_counter_ns()
     duration = end_time - start_time
     assert duration <= param.max_time.total_seconds() * 1e9
