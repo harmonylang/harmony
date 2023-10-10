ef7ffe958c4ce45b5a0c741636c6a6a8bf324e70
Robbert van Renesse <rv22@cornell.edu>
2023-10-10 13:41:52 -0400

    Profiling

--- harmony_model_checker/charm/charm.c
+++ harmony_model_checker/charm/charm.c
@@ -127,6 +127,7 @@ struct results_block {
 
 // One of these per worker thread
 struct worker {
+    int64_t mindiff, maxdiff;
     double timeout;              // deadline for model checker (-t option)
     struct failure *failures;    // list of discovered failures (not data races)
     unsigned int index;          // index of worker
@@ -489,6 +490,15 @@ static void process_step(
     struct dict_assoc *hn = dict_find_new(w->visited, &w->allocator,
                 sc, size, noutgoing * sizeof(struct edge), &new, &lock);
     struct node *next = edge->u.after.dst = (struct node *) &hn[1];
+
+    int64_t diff = (uint64_t *) next - (uint64_t *) node;
+    if (diff < w->mindiff) {
+        w->mindiff = diff;
+    }
+    if (diff > w->maxdiff) {
+        w->maxdiff = diff;
+    }
+
     if (new) {
         assert(node->len == global.diameter);
         next->failed = (edge->u.after.flags & EDGE_FAILED) != 0;
@@ -3722,6 +3732,7 @@ int exec_model_checker(int argc, char **argv){
     printf("    * %u states (time %.2lfs, mem=%.3lfGB)\n", global.graph.size, gettime() - before, (double) allocated / (1L << 30));
     unsigned int si_hits = 0, si_total = 0;
     bool loops_possible = false;
+    int64_t mindiff = 0, maxdiff = 0;
     for (unsigned int i = 0; i < global.nworkers; i++) {
         struct worker *w = &workers[i];
         if (w->loops_possible) {
@@ -3729,8 +3740,15 @@ int exec_model_checker(int argc, char **argv){
         }
         si_hits += w->si_hits;
         si_total += w->si_total;
+        if (w->mindiff < mindiff) {
+            mindiff = w->mindiff;
+        }
+        if (w->maxdiff > maxdiff) {
+            maxdiff = w->maxdiff;
+        }
     }
     printf("    * %u/%u computations/edges\n", (si_total - si_hits), si_total);
+    printf("    * %llx - %llx\n", -mindiff, maxdiff);
 #ifdef HASHDICT_STATS
     float atotal = atomic_load(&visited->nstable_hits) + atomic_load(&visited->nunstable_hits) + atomic_load(&visited->nmisses);
     printf("    * hashtable %.2f/%.2f/%.2f\n", atomic_load(&visited->nstable_hits)/atotal, atomic_load(&visited->nunstable_hits)/atotal, atomic_load(&visited->nmisses)/atotal);
