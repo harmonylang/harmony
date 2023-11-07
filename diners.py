2b55947041eff81e85fd98eea51fba02276493ed
Robbert van Renesse <rv22@cornell.edu>
2023-11-07 15:08:19 -0500

    Latest

--- diners.py
+++ diners.py
@@ -3,8 +3,8 @@ import numpy as np
 
 tlaplus_states = [ 9, 163, 1959, 22847, 287930, 3652847, 46370039 ]
 tlaplus_times = [ 1, 3, 29, 416 ]
-harmony_states = [ 6, 41, 251, 1521, 9095, 53825, 315831, 1840001, 10327210 ]
-harmony_times = [ 0.01, 0.04, 0.25, 2.05, 17 ]
+harmony_states = [ 6, 41, 251, 1521, 9095, 53825, 315831, 1840001, 10327210, 61374849 ]
+harmony_times = [ 0.002, 0.01, 0.07, 0.47, 3.8, 33 ]
 
 fig, ax1 = plt.subplots(figsize=(6, 4))
 ax2 = ax1.twinx()
@@ -15,8 +15,8 @@ ax2.set_yscale("log")
 # ax2.set_xlim(1, 10)
 ts_dim = np.arange(1, 8)
 tt_dim = np.arange(4, 8)
-hs_dim = np.arange(1, 10)
-ht_dim = np.arange(5, 10)
+hs_dim = np.arange(1, 11)
+ht_dim = np.arange(5, 11)
 ax1.set(xlabel="#philosophers", ylabel="#states")
 ax2.set(ylabel="time (secs)")
 ax2.set_yticks([0.01, 0.1, 1, 10, 100], labels=["0.01", "0.1", "1", "10", "100"])
